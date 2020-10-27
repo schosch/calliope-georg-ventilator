be1c4e7eedc5ffdb3bfeed1a5784388196840895
schosch <455119+schosch@users.noreply.github.com>
2020-10-27 23:45:06 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,5 +2,24 @@ let drehzahl = 0
 basic.forever(function () {
     drehzahl = pins.analogReadPin(AnalogPin.P1)
     pins.analogWritePin(AnalogPin.P2, drehzahl)
-    basic.showNumber(drehzahl)
+    if (drehzahl > 0 && drehzahl < 200) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+        if (drehzahl >= 200 && drehzahl < 400) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                # # # # #
+                # # # # #
+                `)
+        }
+    } else {
+        music.playMelody("C D D G E D C - ", 120)
+    }
 })
